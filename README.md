072473ea644a6a127568546999d522083e47dc62
Kyle Foster <hkylefoster@gmail.com>
2013-02-05 15:10:52 -0600

    Added demo, links, and descriptive comments

---
+++ README.md
@@ -0,0 +1,6 @@
+Rift.js
+=========
+
+Just about the lean­est jQuery plu­gin you’ll find (it’s just one chain), Rift uses CSS3 tran­si­tions to split a seman­tic fig­ure ele­ment in twain for a nifty cap­tion reveal. Give it a try!
+
+See a working demo at http://rift.kylefoster.me and usage instructions at http://www.kylefoster.me/projects/rift-js/
---
+++ js/rift.js
@@ -0,0 +1,30 @@
+/**
+ * Rift v1.0.0
+ * @author Kyle Foster
+ * MIT license
+ */
+;(function ( $, window, document, undefined ) {
+
+  $.fn.rift = function () {
+
+    return this.each(function () {
+      
+      // Vurribles
+      var element = $(this),
+          elemImg = element.find('img'),
+          imgSrc  = elemImg.attr('src');
+  
+      // We be chainin'  
+      element
+        .prepend('<span class="top-span"></span>')
+        .append('<span class="btm-span"></span>')
+        .find('span.top-span')
+        .css('background', 'url(' + imgSrc + ') no-repeat center top')
+        .css('background-size', '100%')
+        .parent()
+        .find('span.btm-span')
+        .css('background', 'url(' + imgSrc + ') no-repeat center bottom')
+        .css('background-size', '100%');
+    });
+  };
+})( jQuery, window, document );
