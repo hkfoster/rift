530a1e14276f9868bb26613b7d14ff5a09474a25
Markus Pleines <markus.pleines@virtual-identity.com>
2015-03-04 23:00:35 +0100

    Added easy fallback for touch devices

--- js/rift.js
+++ js/rift.js
@@ -3,36 +3,35 @@
  * @author Kyle Foster
  * MIT license
  */
-;
-(function ($, window, document, undefined) {
+;(function ( $, window, document, undefined ) {
 
-    $.fn.rift = function () {
+  $.fn.rift = function () {
 
-        return this.each(function () {
+    return this.each(function () {
 
-            // Vurribles
-            var element = $(this),
-                elemImg = element.find('img'),
-                imgSrc = elemImg.attr('src');
+      // Vurribles
+      var element = $(this),
+          elemImg = element.find('img'),
+          imgSrc  = elemImg.attr('src');
 
-            // We be chainin'
-            element
-                .prepend('<span class="top-span"></span>')
-                .append('<span class="btm-span"></span>')
-                .find('span.top-span')
-                .css('background', 'url(' + imgSrc + ') no-repeat center top')
-                .css('background-size', '100%')
-                .parent()
-                .find('span.btm-span')
-                .css('background', 'url(' + imgSrc + ') no-repeat center bottom')
-                .css('background-size', '100%');
-        });
-        // Easy fallback for touch devices
-        var isTouchDevice = 'ontouchstart' in document.documentElement;
-        if (isTouchDevice) {
-            element.on('click', function (e) {
-                $(this).toggleClass('open');
-            })
-        }
-    };
-})(jQuery, window, document);
+      // We be chainin'
+      element
+        .prepend('<span class="top-span"></span>')
+        .append('<span class="btm-span"></span>')
+        .find('span.top-span')
+        .css('background', 'url(' + imgSrc + ') no-repeat center top')
+        .css('background-size', '100%')
+        .parent()
+        .find('span.btm-span')
+        .css('background', 'url(' + imgSrc + ') no-repeat center bottom')
+        .css('background-size', '100%');
+    });
+      // Easy fallback for touch devices
+      var isTouchDevice = 'ontouchstart' in document.documentElement;
+      if(isTouchDevice) {
+          element.on('click', function(e) {
+              $(this).toggleClass('open');
+          })
+      }
+  };
+})( jQuery, window, document );
