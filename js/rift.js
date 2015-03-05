18536e1b9b4f680699c5c77c702343aca2ad6ca3
Markus Pleines <markus.pleines@virtual-identity.com>
2015-03-05 21:21:49 +0100

    Touch Fallback (now working correctly)

--- js/rift.js
+++ js/rift.js
@@ -11,8 +11,8 @@
 
       // Vurribles
       var element = $(this),
-          elemImg = element.find('img'),
-          imgSrc  = elemImg.attr('src');
+        elemImg = element.find('img'),
+        imgSrc  = elemImg.attr('src');
 
       // We be chainin'
       element
@@ -25,13 +25,15 @@
         .find('span.btm-span')
         .css('background', 'url(' + imgSrc + ') no-repeat center bottom')
         .css('background-size', '100%');
-    });
+
       // Easy fallback for touch devices
       var isTouchDevice = 'ontouchstart' in document.documentElement;
       if(isTouchDevice) {
-          element.on('click', function(e) {
-              $(this).toggleClass('open');
-          })
+        element.on('click', function(e) {
+          $(this).toggleClass('open');
+        })
       }
+    });
+
   };
 })( jQuery, window, document );
